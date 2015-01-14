e8cff4aa640e3504db7b90cc6ac9319250b0b55a
KarnageKnight <karnageknight@KarnageightsMBP.local.lan>
2015-01-14 23:04:15 +0530

    Initial Commit

---
+++ ATM_codechef/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ATM_codechef
+//
+//  Created by KarnageKnight on 14/01/15.
+//  Copyright (c) 2015 KKCPP. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
